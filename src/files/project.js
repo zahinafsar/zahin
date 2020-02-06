a0c979b81d7e26d5ed0bc9a167318bafd0436826
Mdzahin <afsarzahin@gmail.com>
2020-02-06 06:53:50 -0800

    done

--- src/files/project.js
+++ src/files/project.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import '../css/App.css';
+
+function Project(props){
+	return (
+	<div className="card">
+		<img className="card-img-top" src="" alt="img" />
+		<div className="card-body">
+			<h5 className="card-title">{props.title}</h5>
+			<p className="card-text">{props.text}</p>
+			<a href={props.link} className="btn btn-primary">Visit Site</a>
+		</div>
+	</div>
+      );
+}
 
 const Projects=()=>{
     return (
-<div>
-<h1>Projects</h1>
-</div>
+	<div>
+			<div id="SkillBox">
+				<div><img src="https://img.icons8.com/clouds/500/000000/blueprint.png" alt="img" /></div>
+				<center><h1>All My Projects</h1></center><br/>
+				<center><h6>Not Much Experianced</h6></center>
+				<hr/><br/>
+			</div>
+
+		<div className="card-deck" id="pro">
+
+			<Project 
+			title="Md. Rezaul Haque" 
+			text="That was my first website made for my father.I have used HTML,CSS,JS(simple).This is not a profesional website so you may not get the link"
+			link="http://rezaul.cf/"
+			/>
+
+			<Project 
+			title="Space Shooter Game" 
+			text="Thats was made for our College Science Club.In-fact i am the Front-End developer and graphics designer of this site."
+			link="https://Mdzahin.github.io/games"
+			/>
+
+			<Project 
+			title="Dania cience Club" 
+			text="This is a game made by pure javascript.I think you will enjoy the game.
+			This is also availabe as an android apk file."
+			link="https://daniasc.team/"
+			/>
+
+		</div>
+	</div>
     	)
 }
 
--- src/files/skills.js
+++ src/files/skills.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import '../css/App.css';
+
+
+function Skillbar(props){
+	return (
+	<div className="SkillBar">
+    <div id={props.skillId}> 
+      <span className="Skill-Area ">{props.skill}</span>
+      <span className="PercentText ">{props.level}</span>
+    </div>
+  </div>
+      );
+}
 
 const Skills=()=>{
     return (
-<div>
-<h1>Skills</h1>
+<div id="SkillBox">
+<div><img src="https://img.icons8.com/dusk/512/000000/development-skill.png" alt="skill-icon" /></div>
+<center><h1>Web Developmant Skills</h1></center><hr/><br/>
+
+	<Skillbar skillId="Skill-HTML" skill="HTML" level="99%" />
+
+	<Skillbar skillId="Skill-CSS" skill="CSS" level="99%" />
+
+	<Skillbar skillId="Skill-RJ" skill="React js" level="80%" />
+
+	<Skillbar skillId="Skill-JS" skill="Javascript" level="80%" />
+
+<br/><br/>
+
+
+
+<center><h1>Graphics Design Skills</h1></center><hr/><br/>
+
+	<Skillbar skillId="Skill-photo" skill="Photoshop" level="90%" />
+
+	<Skillbar skillId="Skill-illust" skill="Illustrator" level="90%" />
+
+	<Skillbar skillId="Skill-after" skill="Adobe After Effects" level="70%" />
 </div>
     	)
 }

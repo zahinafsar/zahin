708662a8a5a7474188c9a18922961dd7a55a8c28
Mdzahin <afsarzahin@gmail.com>
2020-02-06 00:18:18 -0800

    done

--- src/files/home.js
+++ src/files/home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../css/App.css';
 
-function Temp(props){
+function HomeTemp(props){
     return (
     <div className="card">
       <img alt="navimg"  src={props.link} />
       <div className="card-body">
-      <p className="card-text">{props.text}</p>
-    </div>
+        <p className="card-text">{props.text}</p>
+      </div>
     </div>
       );
 }
@@ -17,10 +17,10 @@ const Home=()=>{
 <div className="card-deck-me">
 
 <div className="card-deck">
-  <Temp link="./img/about.png" text="Here is written all about me.You will find a complete CV here."/>
-  <Temp link="./img/skill.png" text="My skills are located here so that you can consider about my ability."/>
-  <Temp link="./img/project.png" text="You will find all my projects from here.It will help you to inspect me."/>
-  <Temp link="./img/contacts.png" text="Get all the social links and contact with me."/>
+  <HomeTemp link="./img/about.png" text="Here is written all about me.You will find a complete CV here."/>
+  <HomeTemp link="./img/skill.png" text="My skills are located here so that you can consider about my ability."/>
+  <HomeTemp link="./img/project.png" text="You will find all my projects from here.It will help you to inspect me."/>
+  <HomeTemp link="./img/contacts.png" text="Get all the social links and contact with me."/>
 </div>
 
 </div>

0ed1670ce0e3aaf3000c6a906502bc65260bcf41
Zahin Afsar <43959210+Mdzahin@users.noreply.github.com>
2020-04-08 09:08:00 +0600

    Update nav.js

--- src/files/components/nav.js
+++ src/files/components/nav.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import Navbar from "create-react-nav/nav/navSnack"  
+import React, {Component} from 'react';
+import '../../css/App.css';
+import '../../css/nav.css';
+import {Link} from 'react-router-dom';
 
-const Menubar=()=>{
+class Navbar extends Component {
+		state = {
+			navKey : "key",
+			navChild : "navtoggle",
+			cover : ""
+		}
+
+	navtoogle=()=>{
+		if(this.state.navKey === "key"){
+			this.setState({
+				navKey : "key2",
+				navChild : "navtoggle2",
+				cover : "cover"
+			})
+		}else{
+			this.setState({
+				navKey : "key",
+				navChild : "navtoggle",
+				cover : ""
+			})
+		}
+	}
+	render(){
   return (
-   <Navbar links={[
-      ["/","Home"],
-      ["/about","About"],
-      ["/skills","Skills"],
-      ["/project","Projects"],
-      ["/contact","Contact"]
-    ]}/>
-)
+  	<div>
+    <div className="Navbar">
+       <div className={this.state.navKey} onClick={this.navtoogle}><img alt="navimg"  src="./img/arrow.png" /></div>
+       <div className={this.state.navChild} onClick={this.navtoogle} ><Link className="link" to="/"><img alt="navimg"  src="./img/home.png" />Home</Link></div>
+       <div className={this.state.navChild} onClick={this.navtoogle} ><Link className="link" to="/about"><img alt="navimg"  src="./img/about.png" />About</Link></div>
+       <div className={this.state.navChild} onClick={this.navtoogle} ><Link className="link" to="/skills"><img alt="navimg"  src="./img/skill.png" />Skills</Link></div>
+       <div className={this.state.navChild} onClick={this.navtoogle} ><Link className="link" to="/project"><img alt="navimg"  src="./img/project.png" />Projects</Link></div>
+       <div className={this.state.navChild} onClick={this.navtoogle} ><Link className="link" to="/contact"><img alt="navimg"  src="./img/contacts.png" />Contact</Link></div>
+    </div>
+    <div className={this.state.cover}></div>
+    </div>
+ );
+}
 }
 
 export default Navbar;

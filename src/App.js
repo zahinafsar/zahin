a1afae35ec0ab8b179696b769e03153318366a8b
Zahin Afsar <43959210+Mdzahin@users.noreply.github.com>
2020-05-19 09:39:09 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,14 +40,7 @@ class App extends Component {
        <div id="home">
           <img src="./img/zahin.jpg" alt="zahin" />
           <h1  className="typewriter" id="textanim" style={{textAlign: 'center'}}>Mahir Md. Zahin Afsar</h1><br />
-          <h6 className="name" style={{textAlign: 'center'}}>My name is zahin.This is my personal website and made by myself.<br />Still i am a student.My aim is to be an engineer.<br />I like software as well as electronics.<br />You can get enough information about me from this site.</h6><br />
        </div>
-          <h1 style={{textAlign: 'center'}}>Hire Me From</h1><hr/>
-       <div className="z-btn-group">
-        <Button variant="outline-success"><a href="https://www.upwork.com/o/profiles/users/~0117488527450dd94e/">Upwork</a></Button>
-        <Button variant="outline-info"><a href="https://www.freelancer.com/u/zahinafsar">Frelancer</a></Button>
-        <Button variant="outline-dark"><a href="#footer">Here</a></Button>
-      </div><br />
        <Switch>
           <Route exact path="/" component={Home}/>
           <Route path="/skills" component={Skills}/>
